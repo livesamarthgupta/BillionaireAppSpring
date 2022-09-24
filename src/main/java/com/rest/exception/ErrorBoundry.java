fb66d2d31e586c4ae367acbece6650c99ed5552c
Samarth Gupta <livesamarthgupta@gmail.com>
2022-09-24 18:00:03 +0530

    using JDBCTemplate for DAO

--- src/main/java/com/rest/exception/ErrorBoundry.java
+++ src/main/java/com/rest/exception/ErrorBoundry.java
@@ -1,5 +1,6 @@
 package com.rest.exception;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,6 +10,11 @@ public class ErrorBoundry {
 
     @ExceptionHandler
     public ResponseEntity<?> billionaireNotFound(BillionaireNotFoundException exception) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<?> jdbcBillionaireNotFound(EmptyResultDataAccessException exception) {
         return ResponseEntity.notFound().build();
     }
 }
